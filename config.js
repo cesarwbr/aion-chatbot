0b7cabbfe1507383b767dcdaf01d2bc36b925b1f
Cesar William <cesarwbr@gmail.com>
2014-06-09 23:51:26 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+exports.settings = {
+    "status_message": "I'm a chatbot!",
+    "client": {
+        "jid": "aion.chatbot@gmail.com",
+        "password": process.env.bot_password,
+        "host": "talk.google.com",
+        "port": 5222,
+        "reconnect": true
+    },
+    "allow_auto_subscribe": true,
+    "command_argument_separator": /\s*\;\s*/
+};
