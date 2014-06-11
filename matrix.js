b7780ed97dc5a7ade194ca666e3395069475f012
Rafael Gomes <messaggeroo@gmail.com>
2014-06-11 19:53:38 -0300

    Created Object Matrix

--- matrix.js
+++ matrix.js
@@ -1,18 +1,39 @@
 
+var Matrix = function(){
 
-var split_input = function( options ){
+    this.map = [];
 
-    //Getting all words ( Assuming white space as separator)
-    //Remove all special characteres except white spaces.
-    var row    = options.input.replace(/[^a-zA-Z ]/g, "").trim().split(' ');
-    var map    = [];
-    map[ row ] = options.output;
+    this.insert = function( options ){
 
-    console.log( map );
+        //Getting all words ( Assuming white space as separator)
+        //Remove all special characteres except white spaces.
+        var row    = options.input.replace(/[^a-zA-Z ]/g, "").trim().split(' ');
+        this.map[ row ] = options.output;
+
+    };
+
+    this.best_match = function( ){
+        //TODO
+    };
+
+    this.print = function(){
+        console.log( this.map );
+    };
+
 };
 
 //Testing node matrix.js
-split_input({
+
+var mtx = new Matrix();
+mtx.insert({
                 input:"What is your name?",
                 output:"My name is Teste!"
             });
+
+mtx.insert({
+                input:"How are you?",
+                output:"I´m fine"
+            });
+
+
+mtx.print();
