ab39cb8962cc6f7ef2d0fc6bdf68e44b1895f29a
Cesar William <cesarwbr@gmail.com>
2014-06-15 01:10:08 -0300

    test heroku timeout

--- config.js
+++ config.js
@@ -2,11 +2,12 @@ exports.settings = {
     "status_message": "I'm a chatbot!",
     "client": {
         "jid": "aion.chatbot@gmail.com",
-        "password": process.env.bot_password,
+        "password": "aionchat",
         "host": "talk.google.com",
         "port": 5222,
         "reconnect": true
     },
     "allow_auto_subscribe": true,
-    "command_argument_separator": /\s*\;\s*/
+    "command_argument_separator": /\s*\;\s*/,
+    "herokuUrl": process.env.heroku_url || ""
 };
