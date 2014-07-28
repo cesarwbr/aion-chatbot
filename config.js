d0b98f5681e09cc0132fa1cbb9db219ca3f48a87
Cesar William <cesarwbr@gmail.com>
2014-07-28 12:27:14 -0300

    Fix config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.settings = {
     "status_message": "I'm a chatbot!",
     "client": {
         "jid": "aion.chatbot@gmail.com",
-        "password": "aionchat",
+        "password": process.env.password,
         "host": "talk.google.com",
         "port": 5222,
         "reconnect": true
